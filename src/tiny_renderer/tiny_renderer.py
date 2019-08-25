6c14be890677e1a74c207064a2f7f6df5b1b8698
Guilherme Recchi Cardozo <gcardozo@inf.ufsm.br>
2019-08-25 15:26:17 -0300

    Rasterize triangles and compute basic lighting

--- src/tiny_renderer/tiny_renderer.py
+++ src/tiny_renderer/tiny_renderer.py
@@ -5,8 +5,9 @@ import cv2
 import imgui
 import numpy as np
 
+from math_utils import Vec2, Vec3
 from scene import Scene
-from tiny_renderer.imgui_image import Bitmap
+from tiny_renderer.bitmap import Bitmap
 from tiny_renderer.model import Model
 
 Color = namedtuple("Color", "r g b a")
@@ -24,34 +25,85 @@ class TinyRenderer(Scene):
 
         self._height = 800
         self._width = 800
+        self._depth = 800
+
+        self._scale_x = 1.0
+        self._scale_y = 1.0
+        self._scale_z = 1.0
+
         self._image = np.zeros((self._height, self._width, 3), np.uint8)
+        self._model = None
 
-        self.white = Color(255, 255, 255, 2555)
-        self.red = Color(255, 0, 0, 2555)
+        self.white = Color(255, 255, 255, 255)
+        self.red = Color(255, 0, 0, 255)
+        self.green = Color(0, 255, 0, 255)
+        self.blue = Color(0, 0, 255, 255)
 
-        self._load_model()
+        self.set_scale(0.45, 0.45, 0.45)
+        self.load_model("../resources/african_head.obj")
+        self._fill_triangles()
         self._bitmap = Bitmap(self.get_image())
 
-    def _load_model(self):
-        model = Model()
-        model.load_from_obj("../resources/african_head.obj")
-        for i in range(model.num_faces()):
-            face = model.get_face_at(i)
+    def clear_image(self):
+        self._image = np.zeros((self._height, self._width, 3), np.uint8)
+
+    def set_scale(self, x, y, z):
+        self._scale_x = x
+        self._scale_y = y
+        self._scale_z = z
+
+    def load_model(self, filename):
+        self._model = Model()
+        self._model.load_from_obj(filename)
+
+    def wireframe(self):
+        for i in range(self._model.num_faces()):
+            face = self._model.get_face_at(i)
             for j in range(3):
-                v0 = model.get_vertex_at(face[j])
-                v1 = model.get_vertex_at(face[(j + 1) % 3])
+                v0 = self._model.get_vertex_at(face[j])
+                v1 = self._model.get_vertex_at(face[(j + 1) % 3])
                 # scale down the model
-                x0 = (v0[0] + 1.0) * (self._width * 0.45)
-                y0 = (v0[1] + 1.0) * (self._height * 0.45)
-                x1 = (v1[0] + 1.0) * (self._width * 0.45)
-                y1 = (v1[1] + 1.0) * (self._height * 0.45)
+                x0 = (v0[0] + 1.0) * (self._width * self._scale_x)
+                y0 = (v0[1] + 1.0) * (self._height * self._scale_y)
+                x1 = (v1[0] + 1.0) * (self._width * self._scale_x)
+                y1 = (v1[1] + 1.0) * (self._height * self._scale_y)
 
                 x0, y0, x1, y1 = round(x0), round(y0), round(x1), round(y1)
-                if (x0, y0) == (x1, y1):
-                    continue
                 self.line(x0, y0, x1, y1, self.white)
 
+    def _fill_triangles(self):
+        for i in range(self._model.num_faces()):
+            face = self._model.get_face_at(i)
+            verts = [self._model.get_vertex_at(face[x]) for x in range(3)]
+            # scale the model
+            verts = [
+                Vec3(
+                    (v[0] + 1.0) * (self._width * self._scale_x),
+                    (v[1] + 1.0) * (self._height * self._scale_y),
+                    (v[2] + 1.0) * (self._depth * self._scale_z),
+                )
+                for v in verts
+            ]
+            edge = verts[2].sub(verts[0])
+            other_edge = verts[1].sub(verts[0])
+            face_normal = edge.cross(other_edge).normalized()
+
+            light_dir = Vec3(0, 0, -1)
+            intensity = light_dir.dot(face_normal)
+
+            if intensity > 0:
+                color = Color(255 * intensity, 255 * intensity, 255 * intensity, 255)
+                self.triangle(
+                    Vec2(round(verts[0].x), round(verts[0].y)),
+                    Vec2(round(verts[1].x), round(verts[1].y)),
+                    Vec2(round(verts[2].x), round(verts[2].y)),
+                    color,
+                )
+
     def update(self):
+        if not self._bitmap:
+            return
+
         imgui.begin("Tiny Renderer")
         imgui.image(
             self._bitmap.get_texture_id(), self._bitmap.get_width(), self._bitmap.get_height()
@@ -83,7 +135,8 @@ class TinyRenderer(Scene):
         Draws a line from (x0, y0) to (x1, y1) using Bresenham's algorithm
         """
         if (x0, y0) == (x1, y1):
-            raise ValueError("This is a point, not a line. Use set_pixel instead.")
+            # This is a point, not a line.
+            return
 
         steep = False
         # if the line is steep, we transpose the image
@@ -123,8 +176,59 @@ class TinyRenderer(Scene):
     def set_pixel(self, x, y, color):
         row = y
         col = x
-        self._image[row, col] = (color.b, color.g, color.r)
+        self._image[row, col] = (color.r, color.g, color.b)
 
     def image_to_1_bit_array(self):
         # todo fix this: for now it only checks the blue coordinate (index 0)
         return self._image[:, :, 0] > 0
+
+    def triangle(self, p0: Vec2, p1: Vec2, p2: Vec2, color: Color):
+        """
+        Draws a triangle into self._image
+        """
+        if not self._is_counterclockwise(p0, p1, p2):
+            p0, p1, p2 = (p0, p2, p1) if self._is_counterclockwise(p0, p2, p1) else (p1, p0, p2)
+
+        # create 4 points representing the bounding box of the triangle
+        min_x = min(p0.x, min(p1.x, p2.x))
+        max_x = max(p0.x, max(p1.x, p2.x))
+        min_y = min(p0.y, min(p1.y, p2.y))
+        max_y = max(p0.y, max(p1.y, p2.y))
+
+        for y in range(min_y, max_y + 1):
+            for x in range(min_x, max_x + 1):
+                p = Vec2(x, y)
+                is_inside_triangle = True
+                for edge in [(p0, p1), (p1, p2), (p2, p0)]:
+                    if not self._is_left(p, edge[0], edge[1]):
+                        is_inside_triangle = False
+                        break
+                if is_inside_triangle:
+                    self.set_pixel(x, y, color)
+
+        # draw the triangle border
+        self.line(p0.x, p0.y, p2.x, p2.y, color)
+        self.line(p0.x, p0.y, p1.x, p1.y, color)
+        self.line(p1.x, p1.y, p2.x, p2.y, color)
+
+    def _triangle_area(self, p0, p1, p2):
+        """
+        Returns twice the area - just to avoid division - of the oriented triangle (p0, p1, p2).
+        The area is positive if the triangle is oriented counterclockwise.
+        """
+        return (p1.x - p0.x) * (p2.y - p0.y) - (p1.y - p0.y) * (p2.x - p0.x)
+
+    def _is_counterclockwise(self, p0, p1, p2):
+        """
+        Returns true if the Vec2s p0, p1, p2 are in a counterclockwise order
+        """
+        return self._triangle_area(p0, p1, p2) > 0
+
+    def _is_left(self, p0, p1, p2):
+        """
+        Returns True if p0 is on the left of the line p2 - p1.
+        p ^ <- p is on the left side of the line
+         /
+        /
+        """
+        return self._triangle_area(p0, p1, p2) > 0
